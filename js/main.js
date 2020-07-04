81496b3cd7153af7ffbf817bd56ffabfe403f119
portfollio <67562304+portfollio@users.noreply.github.com>
2020-07-04 21:29:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//корзина
+function eventModal()
+{
+	open.classList.toggle("on-click");
+}
+
+const cardBtn = document.querySelector("#basket");
+const open = document.querySelector(".modal");
+const close = document.querySelector(".btn-close");
+const cancel = document.querySelector(".cancel");
+
+cardBtn.addEventListener("click", eventModal);
+
+close.addEventListener("click", eventModal);
+
+cancel.addEventListener("click", eventModal)
+//длинна анимации
+new WOW().init();
+
+
+// day 1
+
+document.querySelector('.button-auth');
